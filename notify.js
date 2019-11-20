14058add4dc6286ceacb7f9e08610519e83adb9d
Henry Darnell <hjdarnel@users.noreply.github.com>
2019-11-20 16:07:55 -0600

    Init

---
+++ notify.js
@@ -0,0 +1,29 @@
+const { json } = require('micro')
+const Discord = require('discord.js');
+
+module.exports = async (req, res) => {
+    const {text} = await json(req);
+
+    if (!text) {
+        res.writeHead(400, {'Content-Type': 'application/json' }).end();
+        return;
+    }
+
+    const client = new Discord.Client();
+    const TOKEN = process.env.TOKEN;
+    const USER = process.env.USER;
+
+    client.on('ready', async () => {
+        const user = client.users.get(USER);
+
+        if (user.presence.status === "online") {
+            await user.send(text);
+        } else {
+            console.log(`User is ${user.presence.status}`);
+        }
+
+        res.writeHead(200, { 'Content-Type': 'application/json' }).end('sent');
+    });
+
+    client.login(TOKEN);
+};
